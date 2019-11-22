d6b99c2c6f473a44368e5690f8f81d83a2a07e2c
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2019-11-22 11:35:06 +0100

    Updates

---
+++ precache-manifest.7c3fd62fc74dcde5e217a30c685b6fd5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ae2cbde4376ed32276a0498a586b13",
+    "url": "/example-page-with-article/index.html"
+  },
+  {
+    "revision": "41475d62bc561b5fb205",
+    "url": "/example-page-with-article/static/css/main.6e53f4ad.chunk.css"
+  },
+  {
+    "revision": "63c6cbb43da44a7097d5",
+    "url": "/example-page-with-article/static/js/2.f4bf6dd5.chunk.js"
+  },
+  {
+    "revision": "41475d62bc561b5fb205",
+    "url": "/example-page-with-article/static/js/main.882318b7.chunk.js"
+  },
+  {
+    "revision": "03f9c12991a2908aae5f",
+    "url": "/example-page-with-article/static/js/runtime-main.c64b3785.js"
+  },
+  {
+    "revision": "083229b2a1bcb6dca7a05d18061c3241",
+    "url": "/example-page-with-article/static/media/1.083229b2.jpg"
+  },
+  {
+    "revision": "9121b17761df31227e2515737c77422c",
+    "url": "/example-page-with-article/static/media/2.9121b177.jpg"
+  },
+  {
+    "revision": "7a083601358dbbdb2c0ae8b8e09e054e",
+    "url": "/example-page-with-article/static/media/3.7a083601.jpg"
+  },
+  {
+    "revision": "84bf6fd898fb261a276e09f5e289b463",
+    "url": "/example-page-with-article/static/media/4.84bf6fd8.jpeg"
+  }
+]);
